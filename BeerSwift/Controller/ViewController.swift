8be15909febbf49bcc52cf12a97ed41bdd14f882
Ivan Almada <ivan.almada@green-light.com>
2018-01-10 12:38:58 +0100

    Initial commit

---
+++ BeerSwift/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerSwift
+//
+//  Created by Ivan Almada on 10/01/2018.
+//  Copyright © 2018 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: UIViewController`
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
